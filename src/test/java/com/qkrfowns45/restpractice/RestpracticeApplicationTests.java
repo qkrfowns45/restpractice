6727173d3a8ff0ee05ce17c807578cb224ed268f
qkrfowns45 <qkrfowns45@naver.com>
2022-03-19 18:34:56 +0900

    new project start

---
+++ src/test/java/com/qkrfowns45/restpractice/RestpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qkrfowns45.restpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
