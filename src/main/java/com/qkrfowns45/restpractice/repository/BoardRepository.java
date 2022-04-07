cd228a6ab7fb4db5ec99998c0b411306d08a03b5
qkrfowns45 <qkrfowns45@naver.com>
2022-04-07 19:34:59 +0900

    13일차 Api연습 repository에 있는 기능들 사용해보기

--- src/main/java/com/qkrfowns45/restpractice/repository/BoardRepository.java
+++ src/main/java/com/qkrfowns45/restpractice/repository/BoardRepository.java
@@ -3,5 +3,10 @@ package com.qkrfowns45.restpractice.repository;
 import com.qkrfowns45.restpractice.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long>{
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title,String content);
 }
