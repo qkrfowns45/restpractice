9f6ed9790f35af5be81764426d7a7f1390884528
qkrfowns45 <qkrfowns45@naver.com>
2022-04-11 19:23:58 +0900

    14일차 jpa를 이용한 페이지 처리-1

--- src/main/java/com/qkrfowns45/restpractice/controller/BoardController.java
+++ src/main/java/com/qkrfowns45/restpractice/controller/BoardController.java
@@ -4,6 +4,9 @@ import com.qkrfowns45.restpractice.model.Board;
 import com.qkrfowns45.restpractice.repository.BoardRepository;
 import com.qkrfowns45.restpractice.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,8 +26,8 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, Pageable pageable){
+        Page<Board> boards = boardRepository.findAll(pageable);
         model.addAttribute("boards",boards);
         return "board/list";
     }
