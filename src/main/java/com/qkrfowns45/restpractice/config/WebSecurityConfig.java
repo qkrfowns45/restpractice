fd85c89e4fac85725f33b16ae3aa80ffdd68ec95
qkrfowns45 <qkrfowns45@naver.com>
2022-04-18 19:32:50 +0900

    19일차 spring security를 활용한 로그인 처리-2 (passwordencoder사용하려고 하는데 bean오류뜬다 이거 해결해야함)

--- src/main/java/com/qkrfowns45/restpractice/config/WebSecurityConfig.java
+++ src/main/java/com/qkrfowns45/restpractice/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.qkrfowns45.restpractice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
+                    .antMatchers("/").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
@@ -47,8 +48,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .usersByUsernameQuery("select username,password,enabled "
                         + "from user "
                         + "where username = ?")
-                .authoritiesByUsernameQuery("select uwername,name "
-                        + "from authorities "
+                .authoritiesByUsernameQuery("select username,name "
+                        + "from user_role ur inner join user u on ur.user_id = u.id "
+                        + "inner join role r on ur.role_id = r.id "
                         + "where email = ?");
     }
 
